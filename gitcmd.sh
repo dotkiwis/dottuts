1a5532908defd3f7865de666916d01ab25447852
Navaneeth Malingan <navneetnivu07@gmail.com>
2019-05-20 20:45:39 +0530

    complete

--- gitcmd.sh
+++ gitcmd.sh
@@ -2,18 +2,24 @@
 
 git clone git@github.com:dotkiwis/dotsite.git # clone the repo from web
 
+cd dotsite # change to that directory
+
 git checkout "yourbranch" # switch to your branch
+# git checkout carbon
 
 git branch # print the current branch
 
 git status # check add/delete status of current branch
 
 git add -A # add all changes 
-git add "filename with extension" # git add hello.c
+git add "filename with extension" # add specific file
+# git add hello.c
 
 git commit -m "your message" # commit changes
+# git commit -m "new feature"
 
 git push origin "branchname" # push changes to cloud
+# git push origin carbon
 
 git pull # pull latest code
 
