14c8d1e6303f15718c755a7be88b919c3dfbcf2a
Navaneeth Malingan <navneetnivu07@gmail.com>
2019-05-20 20:38:48 +0530

    comments added

--- gitcmd.sh
+++ gitcmd.sh
@@ -1,18 +1,17 @@
 #generate ssh key https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account
 
-git clone git@github.com:dotkiwis/dotsite.git
+git clone git@github.com:dotkiwis/dotsite.git # clone the repo from web
 
+git branch # print the current branch
 
-git branch
+git status # check add/delete status of current branch
 
-git status
-
-git add -A
+git add -A # add all changes 
 git add "filename with extension" # git add hello.c
 
-git commit -m "your message"
+git commit -m "your message" # commit changes
 
-git push origin "branchname"
+git push origin "branchname" # push changes to cloud
 
-git pull
+git pull # pull latest code
 
