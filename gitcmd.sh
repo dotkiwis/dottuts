976e775ddca0b60630c015cf9faf97c536b84b07
Navaneeth Malingan <navneetnivu07@gmail.com>
2019-05-20 20:20:09 +0530

    meg

--- gitcmd.sh
+++ gitcmd.sh
@@ -1,3 +1,8 @@
+#generate ssh key https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account
+
+git clone git@github.com:dotkiwis/dotsite.git
+
+
 git branch
 
 git status
@@ -5,9 +10,9 @@ git status
 git add -A
 git add "filename with extension" # git add hello.c
 
+git commit -m "your message"
+
 git push origin "branchname"
 
 git pull
 
-sgsjgrjg
-jjjjj
